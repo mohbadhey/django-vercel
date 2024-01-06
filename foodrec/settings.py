4ae518889e4c5fccc2ecf633804ae2813e1c0e2c
mohbadhey <110901400+mohbadhey@users.noreply.github.com>
2024-01-06 17:20:23 +0300

    g

--- foodrec/settings.py
+++ foodrec/settings.py
@@ -137,13 +137,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 # STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')],
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#added manually
+# STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'static'),)]
 
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#added manually
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static"
 # ]
